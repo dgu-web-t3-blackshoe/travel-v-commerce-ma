994c34b79a7cf7ef8df4d2e5f7c815161f7ccba0
Hankyul Min <denirokr22@gmail.com>
2023-09-11 10:06:27 +0900

    Init: wanderlust TDD 도입, 모놀리식 리팩터링 프로젝트

---
+++ src/main/java/com/wanderlust/travelvcommerce/TravelVCommerceMonolithicApplication.java
@@ -0,0 +1,13 @@
+package com.wanderlust.travelvcommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelVCommerceMonolithicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelVCommerceMonolithicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/wanderlust/travelvcommerce/TravelVCommerceMonolithicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wanderlust.travelvcommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelVCommerceMonolithicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
